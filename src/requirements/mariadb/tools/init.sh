313d691569b5a30734b803b870c51ae7451718e3
maxi <maxi007velazquez@gmail.com>
2025-12-25 17:31:11 +0100

    Changes

--- src/requirements/mariadb/tools/init.sh
+++ src/requirements/mariadb/tools/init.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
-    echo "Inceptioning MariaDB database"
+    echo "🧱 Initializing MariaDB database..."
 
     mysqld_safe --skip-networking &
     until mysqladmin ping --silent; do
@@ -23,7 +23,7 @@ if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
 
     mysqladmin shutdown -p"${DB_ROOT_PASS}"
 
-    echo "Database initialized"
+    echo "✅ Database initialized successfully."
 fi
 
 exec mysqld_safe
